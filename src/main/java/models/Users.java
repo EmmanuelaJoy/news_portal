9a37d68a45539b4a6aa5acdedf2afb22bc773c8e
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-12 00:09:40 +0300

    add a user class and test class

---
+++ src/main/java/models/Users.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.util.Objects;
+
+public class Users {
+    private int id;
+    private String name;
+    private String department;
+    private String role;
+
+    public Users(int id, String name, String department, String role){
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return id == users.id && name.equals(users.name) && department.equals(users.department) && role.equals(users.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, department, role);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
---
+++ src/test/java/models/UsersTest.java
@@ -0,0 +1,80 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsersTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void instantiatesUserObjectsCorrectly_true() throws Exception {
+        Users users = newUser();
+        assertTrue(users instanceof Users);
+    }
+
+    @Test
+    public void userIdReturnsCorrectly_int() throws Exception {
+        Users users = newUser();
+        assertEquals(1, users.getId());
+    }
+
+    @Test
+    public void userNameReturnsCorrectly_String() throws Exception {
+        Users users = newUser();
+        assertEquals("Joy", users.getName());
+    }
+
+    @Test
+    public void userDepartmentReturnsCorrectly_String() throws Exception {
+        Users users = newUser();
+        assertEquals("IT", users.getDepartment());
+    }
+
+    @Test
+    public void userRoleReturnsCorrectly_String() throws Exception {
+        Users users = newUser();
+        assertEquals("Support", users.getRole());
+    }
+
+    @Test
+    public void setUserIdSetsCorrectly_int() throws Exception {
+        Users users = newUser();
+        users.setId(2);
+        assertNotEquals(1, users.getId());
+    }
+
+    @Test
+    public void setNameSetsCorrectUserName_String() throws Exception {
+        Users users = newUser();
+        users.setName("Ian");
+        assertNotEquals("Joy", users.getName());
+    }
+
+    @Test
+    public void setDepartmentSetsCorrectDepartment_String() throws Exception {
+        Users users = newUser();
+        users.setDepartment("Finance");
+        assertNotEquals("IT", users.getDepartment());
+    }
+
+    @Test
+    public void setRoleSetsCorrectRole_String() throws Exception {
+        Users users = newUser();
+        users.setRole("Balancing ledger accounts");
+        assertNotEquals("Support", users.getRole());
+    }
+
+    private Users newUser() {
+        return new Users(1,"Joy", "IT", "Support");
+    }
+}
